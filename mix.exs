8298e12652a6a9c38858d3485839e3e7159058d0
Dmitry Zhelnin <dmitry.zhelnin@gmail.com>
2018-07-11 11:08:36 +0300

    bump version: 0.2.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Ratekeeper.MixProject do
   def project do
     [
       app: :ratekeeper,
-      version: "0.2.2",
+      version: "0.2.3",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
